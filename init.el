eb687adb415af2da54d842831720e4b9045a4458
moonno317 <139625145+moonno317@users.noreply.github.com>
2024-04-11 18:35:36 +0700

    Add files via upload

--- init.el
+++ init.el
@@ -71,13 +71,8 @@ kernel."
 $" . linux-c-mode)
  auto-mode-alist))
 
-(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-(setq dashboard-center-content t)
-(setq dashboard-show-shortcuts nil)
-(defun dashboard-insert-custom (list-size)
-  (insert "Good Morning"))
-(add-to-list 'dashboard-item-generators  '(custom . dashboard-insert-custom))
-(add-to-list 'dashboard-items '(custom) t)
+
+
 (setq which-key-show-early-on-C-h t)
 (which-key-mode)
 (use-package magit
@@ -132,13 +127,17 @@ $" . linux-c-mode)
 
 (use-package dashboard
   :config
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-banner-logo-title
-        (concat " "
-                (with-temp-buffer
-                  (insert-file-contents "~/.emacs.d/ascii.txt")
-                  (buffer-string)))))
-(setq dashboard-startup-banner nil)
+  (setq dashboard-banner-logo-title "Tomorrow Will Be A Better Day")
+  (setq dashboard-startup-banner "~/Downloads/359895.png")
+  (setq dashboard-center-content t)
+  (setq dashboard-vertically-center-content t)
+  (setq dashboard-show-shortcuts nil)
+  (setq dashboard-items '((recents   . 5)
+                        (bookmarks . 5)
+                        (projects  . 5)
+                        (agenda    . 5)
+                        (registers . 5)))
+  (setq dashboard-navigation-cycle t))
 
 
 (use-package smart-mode-line
@@ -170,29 +169,158 @@ $" . linux-c-mode)
   :init
   (add-hook 'after-init-hook 'sml/setup))
 
-(use-package ewal
-  :init (setq ewal-use-built-in-always-p nil
-              ewal-use-built-in-on-failure-p t
-              ewal-built-in-palette "sexy-material"))
-(use-package ewal-spacemacs-themes
-  :init (progn
-          (setq spacemacs-theme-underline-parens t
-                my:rice:font (font-spec
-                              :family "Source Code Pro"
-                              :weight 'semi-bold
-                              :size 11.0))
-          (show-paren-mode +1)
-          (global-hl-line-mode)
-          (set-frame-font my:rice:font nil t)
-          (add-to-list  'default-frame-alist
-                        `(font . ,(font-xlfd-name my:rice:font))))
-  :config (progn
-            (load-theme 'ewal-spacemacs-modern t)
-            (enable-theme 'ewal-spacemacs-modern)))
 (use-package spaceline
   :after (ewal-evil-cursors winum)
   :init (setq powerline-default-separator nil)
   :config (spaceline-spacemacs-theme))
 
-(set-frame-parameter nil 'alpha-background 75)
-(add-to-list 'default-frame-alist '(alpha-background . 75))
+
+
+(setq doom-modeline-support-imenu t)
+(setq doom-modeline-height 25)
+(setq doom-modeline-bar-width 4)
+(setq doom-modeline-hud nil)
+(setq doom-modeline-window-width-limit 85)
+(setq doom-modeline-project-detection 'auto)
+(setq doom-modeline-buffer-file-name-style 'auto)
+(setq doom-modeline-icon t)
+(setq doom-modeline-major-mode-icon t)
+(setq doom-modeline-major-mode-color-icon t)
+(setq doom-modeline-buffer-state-icon t)
+(setq doom-modeline-buffer-modification-icon t)
+(setq doom-modeline-lsp-icon t)
+(setq doom-modeline-time-icon t)
+(setq doom-modeline-time-live-icon t)
+(setq doom-modeline-time-analogue-clock t)
+(setq doom-modeline-time-clock-size 0.7)
+(setq doom-modeline-unicode-fallback nil)
+(setq doom-modeline-buffer-name t)
+(setq doom-modeline-highlight-modified-buffer-name t)
+(setq doom-modeline-column-zero-based t)
+(setq doom-modeline-percent-position '(-3 "%p"))
+(setq doom-modeline-position-line-format '("L%l"))
+(setq doom-modeline-position-column-format '("C%c"))
+(setq doom-modeline-position-column-line-format '("%l:%c"))
+(setq doom-modeline-minor-modes nil)
+(setq doom-modeline-enable-word-count nil)
+(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
+(setq doom-modeline-buffer-encoding t)
+(setq doom-modeline-indent-info nil)
+(setq doom-modeline-total-line-number nil)
+(setq doom-modeline-vcs-icon t)
+
+;; Whether display the icon of check segment. It respects option `doom-modeline-icon'.
+(setq doom-modeline-check-icon t)
+
+;; If non-nil, only display one number for check information if applicable.
+(setq doom-modeline-check-simple-format nil)
+
+;; The maximum number displayed for notifications.
+(setq doom-modeline-number-limit 99)
+
+;; The maximum displayed length of the branch name of version control.
+(setq doom-modeline-vcs-max-length 12)
+
+;; Whether display the workspace name. Non-nil to display in the mode-line.
+(setq doom-modeline-workspace-name t)
+
+;; Whether display the perspective name. Non-nil to display in the mode-line.
+(setq doom-modeline-persp-name t)
+
+;; If non nil the default perspective name is displayed in the mode-line.
+(setq doom-modeline-display-default-persp-name nil)
+
+;; If non nil the perspective name is displayed alongside a folder icon.
+(setq doom-modeline-persp-icon t)
+
+;; Whether display the `lsp' state. Non-nil to display in the mode-line.
+(setq doom-modeline-lsp t)
+
+;; Whether display the GitHub notifications. It requires `ghub' package.
+(setq doom-modeline-github nil)
+
+;; The interval of checking GitHub.
+(setq doom-modeline-github-interval (* 30 60))
+
+;; Whether display the modal state.
+;; Including `evil', `overwrite', `god', `ryo' and `xah-fly-keys', etc.
+(setq doom-modeline-modal t)
+
+;; Whether display the modal state icon.
+;; Including `evil', `overwrite', `god', `ryo' and `xah-fly-keys', etc.
+(setq doom-modeline-modal-icon t)
+
+;; Whether display the modern icons for modals.
+(setq doom-modeline-modal-modern-icon t)
+
+;; When non-nil, always show the register name when recording an evil macro.
+(setq doom-modeline-always-show-macro-register nil)
+
+;; Whether display the mu4e notifications. It requires `mu4e-alert' package.
+(setq doom-modeline-mu4e nil)
+;; also enable the start of mu4e-alert
+(mu4e-alert-enable-mode-line-display)
+
+;; Whether display the gnus notifications.
+(setq doom-modeline-gnus t)
+
+;; Whether gnus should automatically be updated and how often (set to 0 or smaller than 0 to disable)
+(setq doom-modeline-gnus-timer 2)
+
+;; Wheter groups should be excludede when gnus automatically being updated.
+(setq doom-modeline-gnus-excluded-groups '("dummy.group"))
+
+;; Whether display the IRC notifications. It requires `circe' or `erc' package.
+(setq doom-modeline-irc t)
+
+;; Function to stylize the irc buffer names.
+(setq doom-modeline-irc-stylize 'identity)
+
+;; Whether display the battery status. It respects `display-battery-mode'.
+(setq doom-modeline-battery t)
+
+;; Whether display the time. It respects `display-time-mode'.
+(setq doom-modeline-time t)
+
+;; Whether display the misc segment on all mode lines.
+;; If nil, display only if the mode line is active.
+(setq doom-modeline-display-misc-in-all-mode-lines t)
+
+;; The function to handle `buffer-file-name'.
+(setq doom-modeline-buffer-file-name-function #'identity)
+
+;; The function to handle `buffer-file-truename'.
+(setq doom-modeline-buffer-file-truename-function #'identity)
+
+;; Whether display the environment version.
+(setq doom-modeline-env-version t)
+;; Or for individual languages
+(setq doom-modeline-env-enable-python t)
+(setq doom-modeline-env-enable-ruby t)
+(setq doom-modeline-env-enable-perl t)
+(setq doom-modeline-env-enable-go t)
+(setq doom-modeline-env-enable-elixir t)
+(setq doom-modeline-env-enable-rust t)
+
+;; Change the executables to use for the language version string
+(setq doom-modeline-env-python-executable "python") ; or `python-shell-interpreter'
+(setq doom-modeline-env-ruby-executable "ruby")
+(setq doom-modeline-env-perl-executable "perl")
+(setq doom-modeline-env-go-executable "go")
+(setq doom-modeline-env-elixir-executable "iex")
+(setq doom-modeline-env-rust-executable "rustc")
+
+;; What to display as the version while a new one is being loaded
+(setq doom-modeline-env-load-string "...")
+
+;; By default, almost all segments are displayed only in the active window. To
+;; display such segments in all windows, specify e.g.
+(setq doom-modeline-always-visible-segments '(mu4e irc))
+
+;; Hooks that run before/after the modeline version string is updated
+(setq doom-modeline-before-update-env-hook nil)
+(setq doom-modeline-after-update-env-hook nil)
+
+
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
